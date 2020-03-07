dded6518b4e24d80d4e3d706fe49e48f57c31a57
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-07 20:38:34 +0900

    RSpec, devise, hamlセットアップ

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,23 @@ module OnlineLanguageLesson
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    # 日本語化
+    I18n.available_locales = [:ja]
+    I18n.enforce_available_locales = true
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+    # generatorの設定
+    config.generators do |g|
+      g.test_framework  :rspec, :fixture => true
+      g.view_specs false
+      g.controller_specs false
+      g.routing_specs false
+      g.helper_specs false
+      g.request_specs false
+      g.assets false
+      g.helper false
+    end
   end
 end
