f2817943138aa24fdbe4324defb1f5ce58b916e5
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-08 11:41:39 +0900

    講師がレッスンを登録できるように

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,7 +3,7 @@ class LessonsController < ApplicationController
   before_action :set_lesson, only: [:show, :edit, :update, :destroy]
 
   def index
-    @lessons = Lesson.all
+    @lessons = current_teacher.lessons.recent
   end
 
   def show
@@ -17,10 +17,10 @@ class LessonsController < ApplicationController
   end
 
   def create
-    @lesson = Lesson.new(lesson_params)
+    @lesson = current_teacher.lessons.build(lesson_params)
 
     if @lesson.save
-      redirect_to @lesson, notice: 'Lesson was successfully created.'
+      redirect_to lessons_url, notice: 'レッスンを追加しました'
     else
       render :new
     end
@@ -28,15 +28,15 @@ class LessonsController < ApplicationController
 
   def update
     if @lesson.update(lesson_params)
-      redirect_to @lesson, notice: 'Lesson was successfully updated.'
+      redirect_to lessons_url, notice: 'レッスンを更新しました'
     else
       render :edit
     end
   end
 
   def destroy
-    @lesson.destroy
-    redirect_to lessons_url, notice: 'Lesson was successfully destroyed.'
+    @lesson.destroy!
+    redirect_to lessons_url, notice: 'レッスンを削除しました'
   end
 
   private
@@ -46,6 +46,6 @@ class LessonsController < ApplicationController
   end
 
   def lesson_params
-    params.fetch(:lesson, {})
+    params.require(:lesson).permit(:language_id, :time_table_id)
   end
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,8 @@
 class Lesson < ApplicationRecord
   belongs_to :teacher
   belongs_to :time_table
+  belongs_to :language
+
+  scope :recent, -> { order(created_at: :desc) }
+
 end
