6c499913c3af15507edda993783155c2783f30ee
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-08 10:23:30 +0900

    タイムテーブルモデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_07_120542) do
+ActiveRecord::Schema.define(version: 2020_03_08_012209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,12 @@ ActiveRecord::Schema.define(version: 2020_03_07_120542) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_teachers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
+  end
+
+  create_table "time_tables", force: :cascade do |t|
+    t.time "start_time", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
---
+++ spec/factories/time_tables.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :time_table do
+    start_time { "2020-03-08 10:22:09" }
+  end
+end
