67495f89ed46bd77745d8952027983bced134c05
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-08 09:55:03 +0900

    講師でログインできるように

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root to: "homes#index"
-  devise_for :teachers
+
+  devise_for :teachers, controllers: {
+      sessions:      'teachers/sessions',
+      passwords:     'teachers/passwords',
+      registrations: 'teachers/registrations'
+  }
+
   devise_for :users, controllers: {
       sessions:      'users/sessions',
       passwords:     'users/passwords',
