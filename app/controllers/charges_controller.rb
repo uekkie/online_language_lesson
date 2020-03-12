d6fe575aabb3fee68e904ebd6fe319609e003e2a
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-13 00:11:37 +0900

    以前のカード情報をつかってチケットが購入できるように

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,14 +1,18 @@
 class ChargesController < ApplicationController
   before_action :authenticate_user!
+
   def new
+    @user = current_user
   end
 
   def create
     coupon = Coupon.find(params[:coupon_id])
 
-    customer = current_user.stripe_customer(params[:stripeToken])
+    customer = params[:use_registerd_id].present? ?
+                   current_user.customer :
+                   current_user.attach_customer(params[:stripeToken])
 
-    charge = Stripe::Charge.create(
+    Stripe::Charge.create(
       :customer => customer.id,
       :amount => coupon.price,
       :description => "Onlineレッスンチケット #{coupon.name}",
