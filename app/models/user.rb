b56ae923aaf06a6eca25d679f132cef51069cdf5
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-13 00:11:37 +0900

    以前のカード情報をつかってチケットが購入できるように

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,22 +7,25 @@ class User < ApplicationRecord
   has_many :reservations
   has_many :ticket_balances
 
-  def create_stripe_customer(stripe_token)
-    customer = Stripe::Customer.create(
-      email: email,
-      source: stripe_token
-    )
+  def has_customer_id?
+    stripe_customer_id.present?
   end
 
-  def stripe_customer(stripe_token)
+  def customer
     return Stripe::Customer.retrieve(stripe_customer_id) if stripe_customer_id.present?
+  end
 
+  def attach_customer(stripe_token)
     customer = Stripe::Customer.create(
       email: email,
       source: stripe_token
     )
     self.update_attribute(:stripe_customer_id, customer.id)
     customer
+  end
+
+  def ticket_balance_empty?
+    calc_ticket_balance == 0
   end
 
   def calc_ticket_balance
