9a2dec16364103788cae34347330d7784db880ee
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-12 14:59:01 +0900

    （仮）Stripeからの決済でチケット残高を増やせるように

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,27 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :reservations
+  has_many :ticket_balances
+
+  def create_stripe_customer(stripe_token)
+    customer = Stripe::Customer.create(
+      email: email,
+      source: stripe_token
+    )
+  end
+
+  def stripe_customer(stripe_token)
+    return Stripe::Customer.retrieve(stripe_customer_id) if stripe_customer_id.present?
+
+    customer = Stripe::Customer.create(
+      email: email,
+      source: stripe_token
+    )
+    self.update_attribute(:stripe_customer_id, customer.id)
+    customer
+  end
+
+  def calc_ticket_balance
+    ticket_balances.inject(0) { |result, t| result += t.amount  }
+  end
 end
