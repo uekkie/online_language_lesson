4c69f8e6f6afa205e17916f9f5d8be9677e2fa3d
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-15 00:39:16 +0900

    管理者ユーザーを追加
    - 管理者でログインできるように
    - 講師のCRUDできるように
    - 講師に代理ログインできるように

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,11 @@
 class HomesController < ApplicationController
   def index
     if teacher_signed_in?
-      redirect_to lessons_url
+      if current_teacher.admin?
+        redirect_to teachers_url
+      else
+        redirect_to lessons_url
+      end
     elsif user_signed_in?
       redirect_to reservations_url
     end
