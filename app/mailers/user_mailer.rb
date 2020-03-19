cd337882fa78bf40f2c3a95f28ef8165dd34507e
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-19 14:14:52 +0900

    master.keyをdev環境でリフレッシュしてしまったので、credentials.yml.encを再構築（動作には影響なし）

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'hirocueki@gmail.com'
+
+  def accepted_reservation
+    @user = params[:user]
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: '私の素敵なサイトへようこそ')
+  end
 end
