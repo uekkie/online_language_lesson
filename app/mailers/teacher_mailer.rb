780667f5089f521a9189c98ade41f5508a8ea4d9
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-20 10:23:22 +0900

    メールの宛先の修正

--- app/mailers/teacher_mailer.rb
+++ app/mailers/teacher_mailer.rb
@@ -4,6 +4,6 @@ class TeacherMailer < ApplicationMailer
   def accept_lesson(reservation)
     @lesson = reservation.lesson
     @user = reservation.user
-    mail(to: @user.email, subject: '予約完了メール')
+    mail(to: @lesson.teacher.email, subject: '予約完了メール')
   end
 end
