da87c0def15dd1d0a5acfce8547d3f15af2367f9
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-20 11:30:51 +0900

    管理者用のテスト追加

--- spec/factories/teachers.rb
+++ spec/factories/teachers.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :teacher do
+    name {"鈴木"}
+    sequence(:email){|n| "teacher#{n}@example.com"}
+    password {"password"}
+    password_confirmation {"password"}
+  end
+
+  factory :admin, class: "Teacher" do
+    name {"管理者"}
+    admin { true }
     sequence(:email){|n| "teacher#{n}@example.com"}
     password {"password"}
     password_confirmation {"password"}
