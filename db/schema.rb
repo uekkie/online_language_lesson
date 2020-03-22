94c415b7f9e04db7f5d0420d62cc826d9fe79a36
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-23 00:07:34 +0900

    講師がレッスン可能な言語を設定できるように変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_22_085342) do
+ActiveRecord::Schema.define(version: 2020_03_22_145054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,9 @@ ActiveRecord::Schema.define(version: 2020_03_22_085342) do
     t.string "name", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["name"], name: "index_languages_on_name", unique: true
+    t.bigint "teacher_id", null: false
+    t.index ["teacher_id", "name"], name: "index_languages_on_teacher_id_and_name", unique: true
+    t.index ["teacher_id"], name: "index_languages_on_teacher_id"
   end
 
   create_table "lessons", force: :cascade do |t|
@@ -81,6 +83,7 @@ ActiveRecord::Schema.define(version: 2020_03_22_085342) do
   end
 
   add_foreign_key "coupon_balances", "users"
+  add_foreign_key "languages", "teachers"
   add_foreign_key "lessons", "languages"
   add_foreign_key "lessons", "teachers"
   add_foreign_key "reservations", "lessons"
