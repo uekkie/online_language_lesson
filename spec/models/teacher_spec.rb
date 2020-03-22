648a0abf90d002ce87743fb493071fbd6bc3cf6c
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-23 00:17:16 +0900

    テストの修正

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -2,18 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Teacher, type: :model do
   let!(:teacher) { create(:teacher) }
-  let!(:language) { create(:language) }
-  let!(:lesson) { create(:lesson) }
+  let!(:language) { create(:language, teacher: teacher) }
+  let!(:lesson) { create(:lesson, teacher: teacher, language: language) }
 
   it 'teacherを削除すると、teacherのlessonsも削除されること' do
-    teacher.lessons.create(language: language, start_date: DateTime.now, zoom_url: 'https://zoom.us/12345')
     expect{ teacher.destroy }.to change{ Lesson.count }.by(-1)
   end
 
   it 'teacherを削除すると、teacherのreservationsも削除されること' do
     user = create(:user)
 
-    user.reservations.create(lesson: lesson, teacher: lesson.teacher)
+    user.reservations.create(lesson: lesson)
     expect(lesson.teacher.reservations.count).to eq 1
     expect{ lesson.teacher.destroy }.to change{ lesson.teacher.reservations.count }.by(-1)
   end
