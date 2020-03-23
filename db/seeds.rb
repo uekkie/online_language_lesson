cce53e107c64954570a11710ca38ec242d6f209b
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-24 07:16:26 +0900

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
-if Language.count == 0
-  Language.create(name: '英語')
-  Language.create(name: 'スペイン語')
-  Language.create(name: 'ヒンドゥー語')
-  Language.create(name: 'フランス語')
+if Teacher.count == 0
+  Teacher.create(name: '鈴木', email: 'suzuki@example.com', password: 'aaaaaa', password_confirmation: 'aaaaaa')
+end
+
+if User.count == 0
+  User.create(name: '山田', email: 'yamada@example.com', password: 'aaaaaa', password_confirmation: 'aaaaaa')
 end
 
 if Teacher.where(admin: true).blank?
   Teacher.create(name: 'admin', email: 'admin@example.com', password: 'aaaaaa', password_confirmation: 'aaaaaa', admin: true)
 end
+
