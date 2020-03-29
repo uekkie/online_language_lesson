87f11b7e4cabfb68376ec4ca344e693242ad2d32
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-29 15:16:50 +0900

    予約率の表示を改善
    * セルの色を設定する（青、オレンジ、赤）
    * 予約数/レッスン数を表示する

--- app/controllers/admins/reservations_controller.rb
+++ app/controllers/admins/reservations_controller.rb
@@ -18,11 +18,28 @@ class Admins::ReservationsController < ApplicationController
     reserve_hash = Lesson.where(
       id: Reservation.pluck(:lesson_id)
     ).order(:date, :hour).group(:date, :hour).count
-    
+
     lesson_zero_padding_hash = lesson_hash.keys.map{|k| [k,0]}.to_h
     reserve_zero_padding_hash = lesson_zero_padding_hash.merge(reserve_hash)
 
-    lesson_hash.merge(reserve_zero_padding_hash){|key, lesson, reserve| reserve*100/lesson }
+    lesson_hash.merge(reserve_zero_padding_hash) do |key, lesson, reserve|
+      {
+          lesson_count: lesson,
+          reserve_count: reserve,
+          percentage: reserve*100/lesson,
+          color: cell_color(reserve*100/lesson)
+      }
+    end
   end
 
+  def cell_color(percentage)
+    case percentage
+    when 0..50 then
+      "blue"
+    when 51..80 then
+      "orange"
+    else
+      "red"
+    end
+  end
 end
