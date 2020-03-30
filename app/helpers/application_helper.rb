d36f6798d5c57dac7c262c1458227c543159d983
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-30 18:21:01 +0900

    日別の予約率のグラフを表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,20 @@ module ApplicationHelper
     session[:admin_id].present?
   end
 
+  def td_classes_for(day, start_date)
+    today = Date.current
+
+    td_class = ['day']
+    td_class << "wday-#{day.wday.to_s}"
+    td_class << 'today'         if today == day
+    td_class << 'past'          if today > day
+    td_class << 'future'        if today < day
+    td_class << 'start-date'    if day.to_date == start_date.to_date
+    td_class << 'prev-month'    if start_date.month != day.month && day < start_date
+    td_class << 'next-month'    if start_date.month != day.month && day > start_date
+    td_class << 'current-month' if start_date.month == day.month
+
+    td_class
+  end
+
 end
