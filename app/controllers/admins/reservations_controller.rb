f1eb10d486cb71601061a1aaf9232e5186ee65fa
Hiroshi Ueki <hirocueki@gmail.com>
2020-04-06 18:55:52 +0900

    インスタンスの数を減らした

--- app/controllers/admins/reservations_controller.rb
+++ app/controllers/admins/reservations_controller.rb
@@ -1,19 +1,19 @@
 class Admins::ReservationsController < Admins::ApplicationController
-  before_action :set_date
-  before_action :filtered_lessons
+  before_action :date_range
 
   def index
-    @datas = calc_percentage(@filtered_lessons)
+    @datas = calc_percentage(filtered_lessons)
   end
 
   private
 
-  def set_date
-    @start_date = params[:start_date] ? Date.parse(params[:start_date]) : Date.tomorrow
-    @end_date = params[:end_date] ? Date.parse(params[:end_date]) : 15.days.since.to_date
+  def date_range
+    start_date = params[:start_date] ? Date.parse(params[:start_date]) : Date.tomorrow
+    end_date = params[:end_date] ? Date.parse(params[:end_date]) : 15.days.since.to_date
+    @date_range = start_date..end_date
   end
 
   def filtered_lessons
-    @filtered_lessons = Lesson.where(date: @start_date..@end_date)
+    Lesson.where(date: @date_range)
   end
 end
