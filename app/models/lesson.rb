a12ec5d42e79f985f5004ef14265189fffb01cdf
Hiroshi Ueki <hirocueki@gmail.com>
2020-04-12 23:26:56 +0900

    コントローラからモデルへの責務移譲

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -79,4 +79,40 @@ class Lesson < ApplicationRecord
       }
     end
   end
+
+  def self.teachers_stats(lessons)
+    lessons_group_by_month = lessons.group_by_month(:date, format: "%Y-%m-%d,%b").count
+    reserved_lessons = lessons.where.not(reservation: nil)
+    reserved_lessons_group_by_month = reserved_lessons.group_by_month(:date, format: "%Y-%m-%d,%b").count
+
+    lessons_group_by_month.map do |k,v|
+      reserve_count = reserved_lessons_group_by_month.has_key?(k) ? reserved_lessons_group_by_month[k] : 0
+      {
+          date: k.split(",").first,
+          month:  k.split(",").second,
+          lesson_count: v,
+          reserve_count: reserve_count
+      }
+    end
+  end
+
+  def self.calc_percentage(lessons)
+    lesson_hash = lessons.order(:date, :hour).group(:date, :hour).count
+
+    reserve_hash = lessons.where(
+        id: Reservation.pluck(:lesson_id)
+    ).order(:date, :hour).group(:date, :hour).count
+
+    lesson_zero_padding_hash = lesson_hash.keys.map{|k| [k,0]}.to_h
+    reserve_zero_padding_hash = lesson_zero_padding_hash.merge(reserve_hash)
+
+    lesson_hash.merge(reserve_zero_padding_hash) do |key, lesson, reserve|
+      {
+          lesson_count: lesson,
+          reserve_count: reserve,
+          percentage: reserve*100/lesson,
+          color: cell_color(reserve*100/lesson)
+      }
+    end
+  end
 end
