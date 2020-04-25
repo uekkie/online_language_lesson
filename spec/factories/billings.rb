7a39dfe66fd58623e615671068fe820487d22412
hirocueki <hirocueki@gmail.com>
2020-04-25 12:13:03 +0900

    未使用のファイル削除

--- spec/factories/billings.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :billing do
-    user { nil }
-    plan_id { 1 }
-  end
-end
--- spec/factories/subscriptions.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :subscription do
-    user { nil }
-    plan { nil }
-    suspend { false }
-    start_at { "2020-04-25 07:59:52" }
-  end
-end
